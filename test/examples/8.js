d28800c571f1550f437be479c4b5517b70cc8708
Aleksei Schurak <a@guava.by>
2018-07-02 22:50:06 +0300

    add test case

---
+++ test/examples/8.js
@@ -0,0 +1,6 @@
+import objectPath, { get as objGet, set as objSet } from 'object-path'
+
+objGet(foo, 'bar.baz')
+objGet(foo.bar.baz.bar, 'baz')
+objGet(foo.bar, 'baz.bar')
+objectPath[cond ? 'get' : 'set'](foo, 'bar')
